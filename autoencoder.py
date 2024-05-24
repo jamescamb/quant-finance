34cfae1bc147f296d57572f37d17a71cac067cf9
cambridge-quant <cummins_james@hotmail.com>
2024-05-24 21:51:16 +0100

    Updated autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 import tensorflow as tf
 
@@ -8,7 +9,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from keras import layers, losses
 from tensorflow import keras
-#from keras.datasets import mnist
+from keras.datasets import mnist
 from keras.models import Model
 
 from pypfopt import risk_models
@@ -35,25 +36,39 @@ print(S.head(5))
 print(mu.head(5))
 
 stock_data = stock_data.values.T
+stock_data = stock_data[0]
+
+# Loading the MNIST dataset and extracting training and testing data
+(x_train, _), (x_test, _) = mnist.load_data()
+
+# Normalizing pixel values to the range [0, 1]
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+
+# Displaying the shapes of the training and testing datasets
+print("Shape of the training data:", x_train.shape)
+print("Shape of the testing data:", x_test.shape)
 
 # Definition of the Autoencoder model as a subclass of the TensorFlow Model class
 
 class SimpleAutoencoder(Model):
+
 	def __init__(self,latent_dimensions , data_shape):
+		
 		super(SimpleAutoencoder, self).__init__()
 		self.latent_dimensions = latent_dimensions
 		self.data_shape = data_shape
 
 		# Encoder architecture using a Sequential model
 		self.encoder = tf.keras.Sequential([
-			#layers.Flatten(),
+			layers.Flatten(),
 			layers.Dense(latent_dimensions, activation='relu'),
 		])
 
 		# Decoder architecture using another Sequential model
 		self.decoder = tf.keras.Sequential([
-			layers.Dense(tf.math.reduce_prod(data_shape), activation='sigmoid'),
-			#layers.Reshape(data_shape)
+			layers.Dense(tf.math.reduce_prod(data_shape).numpy(), activation='sigmoid'),
+			layers.Reshape(data_shape)
 		])
 
 	# Forward pass method defining the encoding and decoding steps
@@ -63,16 +78,35 @@ class SimpleAutoencoder(Model):
 		return decoded_data
 
 # Extracting shape information from the testing dataset
-input_data_shape = stock_data.shape[1:]
+input_data_shape = x_test.shape[1:]
 
 # Specifying the dimensionality of the latent space
-latent_dimensions = 10
+latent_dimensions = 25
 
 # Creating an instance of the SimpleAutoencoder model
 simple_autoencoder = SimpleAutoencoder(latent_dimensions, input_data_shape)
 
 simple_autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
 
-simple_autoencoder.fit(stock_data, stock_data,
-				epochs=1,
-				shuffle=True)
+simple_autoencoder.fit(x_train, x_train,
+				epochs=10,
+				shuffle=True,
+				validation_data=(x_test, x_test))
+
+encoded_imgs = simple_autoencoder.encoder(x_test).numpy()
+decoded_imgs = simple_autoencoder.decoder(encoded_imgs).numpy()
+
+n = 6
+plt.figure(figsize=(8, 4))
+for i in range(n):
+	ax = plt.subplot(2, n, i + 1)
+	plt.imshow(x_test[i])
+	plt.title("original")
+	plt.gray()
+
+	# display reconstruction
+	ax = plt.subplot(2, n, i + 1 + n)
+	plt.imshow(decoded_imgs[i])
+	plt.title("reconstructed")
+	plt.gray()
+plt.show()
