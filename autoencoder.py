9667752294e2f120c6002f08d44f1f9e5aaf4c10
cambridge-quant <cummins_james@hotmail.com>
2024-05-25 12:20:46 +0100

    Intermediate update

--- autoencoder.py
+++ autoencoder.py
@@ -1,20 +1,28 @@
-import yfinance as yf
-import pandas as pd
+"""
+Program to implement an autoencoder for stock data
+"""
+
+# Standard libraries
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
+# Neural network libraries
 import tensorflow as tf
-
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn.model_selection import train_test_split
 from keras import layers, losses
-from tensorflow import keras
-from keras.datasets import mnist
 from keras.models import Model
 
+# Financial libraries
+import yfinance as yf
 from pypfopt import risk_models
 from pypfopt import expected_returns
 
+from get_all_tickers import get_tickers as gt
+
+list_of_tickers = gt.get_tickers()
+print(list_of_tickers)
+
+# Import stock data
 symbols = [
     "AAPL", "MSFT", "GOOGL", "AMZN", "META",
     "TSLA", "BRK-B", "V", "JNJ", "WMT", "JPM",
@@ -28,32 +36,32 @@ stock_data = yf.download(
     end="2023-12-31"
 )["Adj Close"]
 
+# Remove rows of missing data
 stock_data = stock_data.dropna()
 
+# Calculate annualised covariance matrix and returns
 S = risk_models.sample_cov(stock_data)
 mu = expected_returns.mean_historical_return(stock_data)
 print(S.head(5))
 print(mu.head(5))
 
+# Split the data into training and testing sets
 stock_data = stock_data.values.T
-stock_data = stock_data[0]
+x_train, x_test = stock_data[0:24], stock_data[24:30]
 
-# Loading the MNIST dataset and extracting training and testing data
-(x_train, _), (x_test, _) = mnist.load_data()
+# Normalise the data ot [0, 1]
+x_train = x_train / np.max(x_train, axis=1)[:, None]
+x_test = x_test / np.max(x_test, axis=1)[:, None]
+print(x_test)
 
-# Normalizing pixel values to the range [0, 1]
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-
-# Displaying the shapes of the training and testing datasets
+# Display the shapes of the training and testing datasets
 print("Shape of the training data:", x_train.shape)
 print("Shape of the testing data:", x_test.shape)
 
 # Definition of the Autoencoder model as a subclass of the TensorFlow Model class
-
 class SimpleAutoencoder(Model):
 
-	def __init__(self,latent_dimensions , data_shape):
+	def __init__(self, latent_dimensions, data_shape):
 		
 		super(SimpleAutoencoder, self).__init__()
 		self.latent_dimensions = latent_dimensions
@@ -79,9 +87,10 @@ class SimpleAutoencoder(Model):
 
 # Extracting shape information from the testing dataset
 input_data_shape = x_test.shape[1:]
+print(input_data_shape)
 
 # Specifying the dimensionality of the latent space
-latent_dimensions = 25
+latent_dimensions = 100
 
 # Creating an instance of the SimpleAutoencoder model
 simple_autoencoder = SimpleAutoencoder(latent_dimensions, input_data_shape)
@@ -89,24 +98,24 @@ simple_autoencoder = SimpleAutoencoder(latent_dimensions, input_data_shape)
 simple_autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
 
 simple_autoencoder.fit(x_train, x_train,
-				epochs=10,
+				epochs=100,
 				shuffle=True,
 				validation_data=(x_test, x_test))
 
 encoded_imgs = simple_autoencoder.encoder(x_test).numpy()
 decoded_imgs = simple_autoencoder.decoder(encoded_imgs).numpy()
 
-n = 6
+n = 5
 plt.figure(figsize=(8, 4))
 for i in range(n):
 	ax = plt.subplot(2, n, i + 1)
-	plt.imshow(x_test[i])
+	plt.plot(x_test[i])
 	plt.title("original")
-	plt.gray()
+	#plt.gray()
 
 	# display reconstruction
 	ax = plt.subplot(2, n, i + 1 + n)
-	plt.imshow(decoded_imgs[i])
+	plt.plot(decoded_imgs[i])
 	plt.title("reconstructed")
-	plt.gray()
+	#plt.gray()
 plt.show()
